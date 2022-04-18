b8a7274e48678f8f130ce0a38a987c7bbbdc0931
SATBIR SINGH <RIBTAS007@OUTLOOK.COM>
2022-04-18 14:59:34 +0530

    Update center_dqn.py

--- center_dqn.py
+++ center_dqn.py
@@ -10,36 +10,31 @@ from gmap import find_pos,j_region
 
 class Center_DQN:
     def __init__(self, state_size, action_size, num_UAV, batch_size):
-        self.state_size = state_size                    # 84,84,1
-        self.action_size = action_size                  # 9
+        self.state_size = state_size                                     # 84,84,1
+        self.action_size = action_size                                   # 9
         self.memory = deque(maxlen=132)
-        #self.memory = []
-        self.gamma = 0.9                                # discount rate
-        self.epsilon = 0.97                             # exploration rate
+        self.gamma = 0.9                                                 # discount rate
+        self.epsilon = 0.97                                              # exploration rate
         self.epsilon_min = 0.05
         self.epsilon_decay = 0.92
-        self.N = 36                                     # exp buffer size
+        self.N = 36                                                      # exp buffer size
         self.rtz = 200
         self.jr = 0
         self.num = 0
-        self.alpha = 0.1                                # update rate
+        self.alpha = 0.1                                                 # update rate
         self.pro = np.zeros([action_size])
         self.loss = []
-        self.model = self._build_model()         # Q nework
-        self.tmodel = self._build_model()        # target value
+        self.model = self._build_model()                                 # Q nework
+        self.tmodel = self._build_model()                                # target value
         self.num_U = num_UAV
-        # for uk in range(num_UAV):       # each UAV has a memory in the form of a deque of size 12 +10 = 22
-            # self.memory.append(deque(maxlen=batch_size+10))
+       
 
-    def _build_model(self):                     # Set network of central training
-                                                # Neural Net for Deep-Q learning Model
+    def _build_model(self):                                              # Set network of central training
+                                                                         # Neural Net for Deep-Q learning Model
         model = keras.Sequential()
         model.add(keras.layers.Conv2D(32, (8, 8), strides=4, activation='relu', input_shape = self.state_size))
-        # model.add(keras.layers.Dropout(0.25))
         model.add(keras.layers.Conv2D(64, (4, 4), strides=2, activation='relu'))
         model.add(keras.layers.Conv2D(64, (3, 3), strides=1, activation='relu'))
-        # model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-        # model.add(keras.layers.Dropout(0.25))
         model.add(keras.layers.Flatten())
         model.add(keras.layers.Dense(512, activation='relu'))
         model.add(keras.layers.Dense(self.action_size, activation='linear'))
@@ -47,63 +42,55 @@ class Center_DQN:
         return model
 
     def remember(self, state, action, reward, next_state, uk):
-        # self.memory[uk].append((state, action, reward, next_state))
         self.memory.append((state, action, reward, next_state))
 
-    def act(self, state, fg):  # state = 84 x84 x 1
+    def act(self, state, fg):                                              # state = 84 x84 x 1
         nrd=np.random.rand()
         if nrd <= self.epsilon:
-            return random.randrange(self.action_size) # take a random action
+            return random.randrange(self.action_size)                      # take a random action
+        state=np.reshape(state, [1, self.state_size[0], self.state_size[1], self.state_size[2]]) # 1 x 84 x 84 x 1
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])                                    # returns action
+
+    def actv(self, state, fg):                                             # state = 84 x84 x 1
+        ep_val = 0.3
+        nrd=np.random.rand()
+        if nrd <= ep_val:
+            return random.randrange(self.action_size)                      # take a random action
 
         state=np.reshape(state, [1, self.state_size[0], self.state_size[1], self.state_size[2]]) # 1 x 84 x 84 x 1
         act_values = self.model.predict(state)
-        return np.argmax(act_values[0])  # returns action
+        return np.argmax(act_values[0])                                    # returns action
 
-    def actv(self, state, fg):  # state = 84 x84 x 1
-        state=np.reshape(state, [1, self.state_size[0], self.state_size[1], self.state_size[2]]) # 1 x 84 x 84 x 1
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])  # returns action
+# ----------------------------------------------------------------------------------------------------------------------
+# Training process
+# ----------------------------------------------------------------------------------------------------------------------
 
-#training process
     def replay(self, batch_size, i1,t):
         self.alpha=1/np.sqrt((t+1)/5)
-
         if self.num==0:
             self.model.save_weights("./save/temp.h5")
             self.tmodel.load_weights("./save/temp.h5")
-
-       # minibatch = random.sample(self.memory[i1], batch_size)
         minibatch = random.sample(self.memory, batch_size)
-
         train_sp=np.zeros([batch_size,self.state_size[0],self.state_size[1],self.state_size[2]])
         tg=np.zeros([batch_size,self.action_size]) # 12 x 9
-
-#        minibatch=self.memory[i]
         error=0
         i=0
-        for state1, action, reward, next_state in minibatch: # 12
-            # (s,a,r, s')
+        for state1, action, reward, next_state in minibatch: # 12  # (s,a,r, s')
             state=np.reshape(state1,[1,self.state_size[0],self.state_size[1],self.state_size[2]]) # 1 x 84 x 84 x 1
             next_state=np.reshape(next_state,[1,self.state_size[0],self.state_size[1],self.state_size[2]])
-            pdc=self.model.predict(state)[0] 
-            # state -> argmax(action) 
-             
+            pdc=self.model.predict(state)[0]  # state -> argmax(action) 
             self.pro[action]+=1
             w=sum(self.pro)/self.pro[action]
-#            if reward<=0:
-#                w=6
             ap=min(0.9,self.alpha*w)
-#            ap=self.alpha
+#           ap=self.alpha
             target = ap*(reward + self.gamma * np.amax(self.tmodel.predict(next_state)[0]))+(1-ap)*pdc[action] 
-
             target_f = self.model.predict(state)
             target_f[0][action] = target
             tg[i]=target_f[0]
             train_sp[i]=state1
             i+=1
             error+=  abs((target-pdc[action])/ap)
-#            self.model.fit(state, target_f, epochs=1, verbose=0)
-        
         self.loss.append(error/batch_size)    
         self.model.fit(train_sp, tg, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min and i1==self.num_U-1:
@@ -112,50 +99,31 @@ class Center_DQN:
         self.jr +=1
         if self.num==self.N:
             self.num=0
-#        if self.jr==self.rtz:
-#            self.jr=0
-#            for i in range(self.action_size):
-#                self.pro[i]=0
         return error/batch_size
 
     def vreplay(self, batch_size, i1,t):
         self.alpha=1/np.sqrt((t+1)/5)
-
         if self.num==0:
             self.tmodel.load_weights("./save/temp.h5")
-
-        # minibatch = random.sample(self.memory[i1], batch_size)
-        minibatch = random.sample(self.memory, batch_size)
-
+        minibatch = random.sample(self.memory, batch_size) 
         train_sp=np.zeros([batch_size,self.state_size[0],self.state_size[1],self.state_size[2]])
         tg=np.zeros([batch_size,self.action_size]) # 12 x 9
-
-#        minibatch=self.memory[i]
         error=0
         i=0
-        for state1, action, reward, next_state in minibatch: # 12
-            # (s,a,r, s')
+        for state1, action, reward, next_state in minibatch: # 12  # (s,a,r, s')
             state=np.reshape(state1,[1,self.state_size[0],self.state_size[1],self.state_size[2]]) # 1 x 84 x 84 x 1
             next_state=np.reshape(next_state,[1,self.state_size[0],self.state_size[1],self.state_size[2]])
-            pdc=self.model.predict(state)[0] 
-            # state -> argmax(action) 
-             
+            pdc=self.model.predict(state)[0]  # state -> argmax(action) 
             self.pro[action]+=1
             w=sum(self.pro)/self.pro[action]
-#            if reward<=0:
-#                w=6
-#            ap=min(0.9,self.alpha*w)
             ap=self.alpha
             target = ap*(reward + self.gamma * np.amax(self.tmodel.predict(next_state)[0]))+(1-ap)*pdc[action] 
-
             target_f = self.model.predict(state)
             target_f[0][action] = target
             tg[i]=target_f[0]
             train_sp[i]=state1
             i+=1
-            error+=  abs((target-pdc[action])/ap)
-#            self.model.fit(state, target_f, epochs=1, verbose=0)
-        
+            error+=  abs((target-pdc[action])/ap)       
         self.loss.append(error/batch_size)    
         self.model.evaluate(train_sp, tg, verbose=0)
         if self.epsilon > self.epsilon_min and i1==self.num_U-1:
@@ -164,10 +132,6 @@ class Center_DQN:
         self.jr +=1
         if self.num==self.N:
             self.num=0
-#        if self.jr==self.rtz:
-#            self.jr=0
-#            for i in range(self.action_size):
-#                self.pro[i]=0
         return error/batch_size
 
     def find_ld(self,UAVlist,alfmin):
@@ -188,7 +152,6 @@ class Center_DQN:
 
     def cal_com(self,UAVlist,alfmin,ite=20):
         [ld_L,ld_U]=self.find_ld(UAVlist,alfmin)
-#        print("%f,%f"%(ld_L,ld_U))
         num=len(UAVlist)
         ite2=20
         for i in range(ite2):
@@ -204,21 +167,18 @@ class Center_DQN:
                 ld_U=mid
         return mid   
     
-    def para_com(self,UAVlist,noise,V,p_max,alfmin):   #calculate UAV offloading
+    def para_com(self,UAVlist,noise,V,p_max,alfmin):                    #calculate UAV offloading
         num=len(UAVlist)
         for i in range(num):
-            UAVlist[i].p_tr=p_max #cal ptr give values to noise....
-            
+            UAVlist[i].p_tr=p_max                                       #cal ptr give values to noise.... 
         for j in range(2):
-            self.cal_com(UAVlist,alfmin)  #cal ptr and alpha by dual decomposition
+            self.cal_com(UAVlist,alfmin)                                #cal ptr and alpha by dual decomposition
             for i in range(num):
                 UAVlist[i].cal_ptr(p_max,V,noise)
         for i in range(num):
             UAVlist[i].cal_f(V)
         return j
             
-        
-    
     def load(self, name):
         self.model.load_weights(name)
 
